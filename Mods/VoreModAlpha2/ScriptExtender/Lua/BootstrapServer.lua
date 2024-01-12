d0cfe18ad8179b2a413a59a6bff388b88d5c21f9
Theodore Calabrese <tcalabre@mit.edu>
2024-01-11 23:34:39 -0500

    Started work on hotloading

--- Mods/VoreModAlpha2/ScriptExtender/Lua/BootstrapServer.lua
+++ Mods/VoreModAlpha2/ScriptExtender/Lua/BootstrapServer.lua
@@ -1,3 +1,16 @@
+StatPaths={
+    "Public/ModName/Stats/Generated/Data/FileName1.txt",
+    "Public/ModName/Stats/Generated/Data/FileName2.txt",
+    "Public/ModName/Stats/Generated/Data/FileName3.txt",
+    "Public/ModName/Stats/Generated/Data/FileName4.txt",
+    "Public/ModName/Stats/Generated/Data/FileName5.txt",
+    "Public/ModName/Stats/Generated/Data/FileName6.txt",
+    "Public/ModName/Stats/Generated/Data/FileName7.txt",
+    "Public/ModName/Stats/Generated/Data/FileName8.txt",
+    "Public/ModName/Stats/Generated/Data/FileName9.txt",
+    "Public/ModName/Stats/Generated/Data/FileName10.txt",
+}
+
 
 PersistentVars = {}
 PredPreyTable = {}
@@ -9,20 +22,20 @@ local function SP_Regurgitate(caster, spell)
         Osi.RemoveSpell(caster, 'SP_Regurgitate', 0)
         local predX, predY, predZ = Osi.getPosition(caster)
         local predXRotation, predYRotation, predZRotation = Osi.getRotation(caster)
-        _P(predX, predY, predZ)
-        _P(predXRotation, predYRotation, predZRotation)
         predYRotation = predYRotation * math.pi / 180
+        _P("The table, sir:")
         _D(PredPreyTable)
         for k, v in pairs(PredPreyTable[caster]) do
-            Osi.TeleportToPosition(v, predX+5*math.cos(predYRotation), predY, predZ+5*math.sin(predYRotation))
+            _P(v)
+            Osi.TeleportToPosition(v, predX+2*math.cos(predYRotation), predY, predZ+2*math.sin(predYRotation))
             Osi.RemoveStatus(v, 'SP_Vore_Swallowed_Endo')
             PredPreyTable[caster][k] = 'deleteme'
         end
+        PredPreyTable[caster] = ArrayRemove(PredPreyTable[caster])
         if next(PredPreyTable[caster]) == nil then
             table.remove(PredPreyTable, caster)
         end
-        ArrayRemove(PredPreyTable[caster])
-        PersistentVars['PredPreyTable'] = PredPreyTable
+        PersistentVars['PredPreyTable'] = table.deepcopy(PredPreyTable)
         _D(PredPreyTable)
     end
 end
@@ -38,7 +51,7 @@ local function SP_fillPredPreyTable(caster, target, spell)
         table.insert(PredPreyTable[caster], target)
         _P('step 3')
         Osi.AddSpell(caster, 'SP_Vore_Regurgitate', 1, 0)
-        PersistentVars['PredPreyTable'] = PredPreyTable
+        PersistentVars['PredPreyTable'] = table.deepcopy(PredPreyTable)
         _D(PredPreyTable)
 
 
@@ -47,7 +60,15 @@ end
 
 function OnSessionLoaded()
     -- Persistent variables are only available after SessionLoaded is triggered!
-    _D('PersistenVars: ' .. PersistentVars)
+    _D(PersistentVars)
+    PredPreyTable = table.deepcopy(PersistentVars['PredPreyTable'])
+end
+
+local function on_reset_completed()
+    for _, statPath in ipairs(StatPaths) do
+        Ext.Stats.LoadStatsFile(statPath,1)
+    end
+    _P('Reloading stats!')
 end
 
 function SP_getAllPreds()
@@ -77,7 +98,29 @@ function ArrayRemove(t)
     return t;
 end
 
+function table.deepcopy(orig, copies)
+    copies = copies or {}
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        if copies[orig] then
+            copy = copies[orig]
+        else
+            copy = {}
+            copies[orig] = copy
+            for orig_key, orig_value in next, orig, nil do
+                copy[table.deepcopy(orig_key, copies)] = table.deepcopy(orig_value, copies)
+            end
+            setmetatable(copy, table.deepcopy(getmetatable(orig), copies))
+        end
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
 
 Ext.Osiris.RegisterListener("UsingSpellOnTarget", 6, "after", SP_fillPredPreyTable)
 Ext.Osiris.RegisterListener("CastedSpell", 5, "after", SP_Regurgitate)
 Ext.Events.SessionLoaded:Subscribe(OnSessionLoaded)
+Ext.Events.ResetCompleted:Subscribe(on_reset_completed)
