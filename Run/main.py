84a0c9072ee24738f475903fc2f58dbc4d3565f7
Acolyte Neko <104366233+Acolyte-Neko@users.noreply.github.com>
2024-08-10 23:24:31 +0300

    automatic compilation preparation

--- Run/main.py
+++ Run/main.py
@@ -1,3 +1,18 @@
 import os
-path = '"S:\SteamLibrary\steamapps\common\Baldurs Gate 3/bin/bg3.exe" --skip-launcher'
-os.system(path)
+import subprocess
+
+def pak():
+    modFolder = os.getenv('LOCALAPPDATA') + "/Larian Studios/Baldur's Gate 3/Mods/"
+    mod = "DevouringAndDigesting"
+    
+    
+    
+    subprocess.check_call("")
+
+def runBG3():
+    import webbrowser
+    path = 'steam://rungameid/1086940'
+    webbrowser.open(path)
+
+pak()
+#runBG3()
